ecb121da37050e5c24886d70290be5eb4975cc83
selamhalefom <43986813+selamhalefom@users.noreply.github.com>
2020-10-08 16:45:09 +0300

    Update guessinggame.sh

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,1 +1,28 @@
+guessing game.sh
+ 
+#!/bin/bash
+echo "learning unix is going so well"
+number=$(ls -l | wc -l)
+
+function func { 
+	echo "Put some input"
+    
+	read IP
+
+}
+
+func
+while [[ $IP -ne $numeroarchivos ]]
+do
+	if [[ $IP -gt $numeroarchivos ]]
+	then
+		echo "Muy arriba"
+		
+	else
+		echo "Muy abajo"
+	fi
+	func
+done
+
+echo "Muy bien! Haz adivinado con exito el numero de archivos!"
 
